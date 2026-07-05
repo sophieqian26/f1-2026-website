383aea94998576ab4ab124c53563ffb511b9db20
Sophie Qian <sophieqian@users.noreply.github.com>
2026-07-05 16:24:02 +0800

    Restore Firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 window.F1_FIREBASE_CONFIG = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyBiqMxxXEhDGUc7cVGw3E56kKRykCmh2XU',
+  authDomain: 'f1f1f1-ab2c2.firebaseapp.com',
+  projectId: 'f1f1f1-ab2c2',
+  storageBucket: 'f1f1f1-ab2c2.firebasestorage.app',
+  messagingSenderId: '492162912616',
+  appId: '1:492162912616:web:718ee50ea50e21cba606dd',
+  measurementId: 'G-B7D3RTJ7BY'
 };
